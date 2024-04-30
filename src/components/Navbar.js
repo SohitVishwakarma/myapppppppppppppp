25801ebe3e09bbfa164e71acc7d34049bdd0b2c3
akash <akash@arivani.net>
2024-04-30 12:16:46 +0530

    dew

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    if (2>3) {
+      <h1>Hello
+
+      </h1>  
+    } else {
+      <h1>else</h1>  
+    }
+   
+  return (
+    <div>
+      <h1 className=' text-2xl text-red-500'>Navbar</h1>
+     
+    </div>
+  )
+}
+
+export default Navbar
